e5b89221766bcdbdec97502f1e5dfe6cbfe20251
Silviu Straliciuc <silviu.straliciuc@gmail.com>
2015-04-23 23:01:16 +0300

    got rid of the monster function, added human

--- entry.js
+++ entry.js
@@ -1,43 +1,47 @@
 ﻿s = 2;//scale
-monsterDirection = 1; //all the monsters are moving in the same direction
-monsterXOffset = 1; //all the monsters are moving at the same time
-monsterFrame = 0; // all the monsters have two frames
+invaderDirection = 1; //all the invaders are moving in the same direction
+invaderXOffset = 1; //all the invaders are moving at the same time
+invaderFrame = 0; // all the invaders have two frames
 	
-mframes1 = ["¾m=<=m¾","x¾l<<<l¾x"];
-mframes2 = [":múúm:","=hüüh="];
-mframes3 = ["9yznìúúìnzy9",":}lìúúìl}:"];
-	
+skins = ["�", ":múúm:", "=hüüh=", "¾m=<=m¾", "x¾l<<<l¾x", "9yznìúúìnzy9", ":}lìúúìl}:"]
 
 update = function(){
-	c.clearRect(0,0,a.width,a.height)
-	for(var i = 0;i<monsters.length;i++){
-		monsters[i].update();
+    c.fillStyle = 0;
+    c.fillRect(0, 0, a.width, a.height)
+
+    //print invaders
+	for(var i = 0;i<invaders.length;i++){
+	    ship = invaders[i];
+        
+	    skin = 0;
+
+	    if(ship.t == 1){
+	        skin = 1 + invaderFrame;
+	    }else if(ship.t == 2){
+	        skin = 3 + invaderFrame;
+	    }else if(ship.t == 3){
+	        skin = 5 + invaderFrame;
+	    }
+
+	    print(ship.x, ship.y, skins[skin]);
+
+	    invaders[i].x += invaderDirection;
 	}
-	monsterFrame = monsterFrame==0?1:0;
-	if(monsterXOffset == 0 || monsterXOffset == 100){
-		monsterDirection *= -1; y+=10;
+
+    //make invaders go to next frame
+	invaderFrame = invaderFrame == 0 ? 1 : 0;
+
+    //make invaders switch direction and return
+	if(invaderXOffset == 0 || invaderXOffset == 100){
+	    invaderDirection *= -1;
+	    invaders.map(function (i) { i.y += 10 });
 	}
-	monsterXOffset+=monsterDirection
+	invaderXOffset += invaderDirection
+
+	print(human.x, human.y, skins[0]);
 }
 
-monster = function(x,y,frames){
-	this.x = x;
-	this.y = y;
-	this.frames = frames;	
-	return{
-		update:function(){
-
-			x += monsterDirection;;
-			
-			p(x,y,frames[monsterFrame]);
-		}
-		//collide:function(){
-		
-		//}
-	}
-}
-
-p = function(x,y,b){
+print = function(x,y,b){
 	x=x||0; y=y||0;
 	for (i = 0;i<b.length;i++){
 		bitIndex = 8;
@@ -45,7 +49,7 @@ p = function(x,y,b){
 		while(bitIndex>0){
 			if(v>=0){
 				if(v%2 == 1)
-					c.fillRect(x*s,(y+bitIndex)*s,s,s);
+					c.clearRect(x*s,(y+bitIndex)*s,s,s);
 				v = Math.floor(v/2);
 			}
 			bitIndex -= 1;
@@ -54,15 +58,21 @@ p = function(x,y,b){
 	}
 }
 
-monsters = [];
+invaders = [];
 for (var i = 0; i < 11; i++) {
-    monsters.push(monster(i * 16 + 2, 0, mframes2));
-    monsters.push(monster(i * 16, 10, mframes1));
-    monsters.push(monster(i * 16, 20, mframes1));
-    monsters.push(monster(i * 16, 30, mframes3));
-    monsters.push(monster(i * 16, 40, mframes3));
+    invaders.push( { x:i * 16 + 2, y: 0, t:1 } );
+    invaders.push( { x:i * 16, y: 10, t: 2 });
+    invaders.push( { x:i * 16, y: 20, t: 2 });
+    invaders.push( { x:i * 16, y: 30, t: 3 });
+    invaders.push({ x: i * 16, y: 40, t: 3 });
 }
-
+human = { x: 0, y: 150, t: 0 }
 
 //debugger;
 setInterval(update,300);
+
+onkeydown = function (k) {
+    w = k.which;
+    (w == 37)?human.x--:(w == 39)?human.x++:"";
+};
+
